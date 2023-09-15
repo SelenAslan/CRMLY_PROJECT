073c2e932844e303660e1aeb064165f5e2288790
Mustafa Emre Atci <m.emreatci@gmail.com>
2023-09-15 23:37:26 +0300

    Mustafa/9/15/V.2

---
+++ src/test/java/com/crmly/pages/ActiveStreamPage.java
@@ -0,0 +1,95 @@
+package com.crmly.pages;
+
+import com.crmly.utilities.BrowserUtils;
+import com.crmly.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ActiveStreamPage extends Base{
+
+    Actions actions = new Actions(Driver.getDriver());
+    Random random = new Random();
+    @FindBy(xpath = "//*[text()='Poll']")
+    public WebElement pollTab;
+    @FindBy(xpath = "//*[text()='Add more']")
+    public WebElement activeStreamPageAddMoreButton;
+    @FindBy(xpath = "//*[text()='Employees and departments']")
+    public WebElement employeesAndDepartmentsLink;
+    @FindBy(xpath = "//*[@class='bx-finder-company-department-employee-info']")
+    public List<WebElement> employeesAndDepartmenList;
+    @FindBy(xpath = "//*[@data-type='users']")
+    public List<WebElement> listOfAdditions;
+    @FindBy(xpath = "//*[@class='bx-editor-iframe']")
+    public WebElement ASPFrameElement;
+    @FindBy(xpath = "//*[@style='min-height: 184px;']")
+    public WebElement ASPQuestionsTitleTextBox;
+    @FindBy(xpath = "//*[@id='question_0']")
+    public WebElement ASPQuestionsTextBox;
+    @FindBy(xpath = "//*[@id='answer_0__0_']")
+    public WebElement ASPAnswerTextBox;
+    @FindBy(xpath = "//*[@id='blog-submit-button-save']")
+    public WebElement ASPSendButton;
+    @FindBy(xpath = "//*[text()='Stop']")
+    public WebElement ASPStopButton;
+    @FindBy(xpath = "//*[@id='answer_0__1_']")
+    public WebElement ASPAnswer2TextBox;
+    @FindBy(xpath = "//*[@id='answer_0__2_']")
+    public WebElement ASPAnswer3TextBox;
+    @FindBy(xpath = "//*[@class='bx-vote-bar']")
+    public List<WebElement> ASPAnswerList;
+    @FindBy(linkText = "More")
+    public WebElement APSMoreLink;
+    @FindBy(xpath = "(//*[@class='menu-popup-item menu-popup-no-icon '])[7]")
+    public WebElement APSMoreLinkDeleteTab;
+    @FindBy(xpath = "(//*[@class='menu-popup-item menu-popup-no-icon '])[5]")
+    public WebElement APSMoreLinkEditTab;
+    @FindBy(xpath = "//*[text()='The post has been deleted.']")
+    public WebElement APSSuccessDeletedText;
+    @FindBy(xpath = "//label[@class='vote-block-close dela']")
+    public List<WebElement> APSEditAnswerIconList;
+    @FindBy(xpath = "//*[@class='vote-block-inp-wrap']")
+    public List<WebElement> APSEditAnswerTextBox;
+    @FindBy(xpath = "(//*[text()='Send'])[2]")
+    public WebElement APSEditAnswerSendButton;
+    @FindBy(xpath = "//*[text()='Allow multiple choice']")
+    public WebElement ASPAllowMultipleChoiceCheckbox;
+    @FindBy(xpath = "//*[text()='The message title is not specified']")
+    public WebElement ASPRequiredFieldTextAlert;
+
+    public void randomPerson(){
+
+        List<String> empAndDepartList = new ArrayList<>();
+
+        for (WebElement eachPerson : employeesAndDepartmenList){
+            empAndDepartList.add(eachPerson.getText());
+        }
+
+        int randomIndex = random.nextInt(empAndDepartList.size());
+
+        String randomPerson = empAndDepartList.get(randomIndex);
+
+        for (WebElement person : employeesAndDepartmenList) {
+
+            if (person.getText().equals(randomPerson)) {
+
+                actions.moveToElement(person).perform();
+                actions.click(person).perform();
+                BrowserUtils.sleep(2);
+
+            }
+        }
+    }
+
+    public void APSEditAnswerDel(){
+        APSEditAnswerTextBox.get(0).click();
+        APSEditAnswerIconList.get(0).click();
+        Driver.getDriver().switchTo().alert().accept();
+        BrowserUtils.sleep(2);
+    }
+}
--- src/test/java/com/crmly/step_def/ActiveStreamStepDef.java
+++ src/test/java/com/crmly/step_def/ActiveStreamStepDef.java
@@ -1,90 +1,154 @@
 package com.crmly.step_def;
 
-import com.crmly.pages.ActivityStreamPage;
+import com.crmly.pages.ActiveStreamPage;
+import com.crmly.utilities.BrowserUtils;
+import com.crmly.utilities.Driver;
+import com.github.javafaker.Faker;
 import io.cucumber.java.en.Given;
+import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class ActiveStreamStepDef {
 
-    ActivityStreamPage activityStreamPage = new ActivityStreamPage();
+    ActiveStreamPage activeStreamPage = new ActiveStreamPage();
+    Faker faker = new Faker();
+    Actions actions = new Actions(Driver.getDriver());
+    JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
 
     @Given("Click the Poll tab under Active Stream.")
     public void click_the_poll_tab_under_active_stream() {
-        try {
-            activityStreamPage.pollTab.click();
-        } catch (Exception e) {
-            throw new RuntimeException("There is no \"poll\" tab under Active Stream !");
-        }
-    }
-
-    @Given("Click the new record.")
-    public void click_the_new_record() {
-
-    }
-
-    @Given("More than one person is selected from the contact lists of the employees and the Department and the add button is clicked.")
-    public void more_than_one_person_is_selected_from_the_contact_lists_of_the_employees_and_the_department_and_the_add_button_is_clicked() {
-
+        activeStreamPage.pollTab.click();
+        BrowserUtils.sleep(2);
     }
 
     @Given("User closes page.")
     public void user_closes_page() {
-
+        Driver.closeDriver();
     }
 
     @Given("The addition of new users is verified.")
     public void the_addition_of_new_users_is_verified() {
-
+        Assert.assertTrue(activeStreamPage.listOfAdditions.size() > 1);
     }
 
-    @Given("New questions are entered and the save button is clicked.")
+    @Given("Enter questions title, questions and answer information. Send button is clicked.")
     public void new_questions_are_entered_and_the_save_button_is_clicked() {
 
+        Driver.getDriver().switchTo().frame(activeStreamPage.ASPFrameElement);
+        activeStreamPage.ASPQuestionsTitleTextBox.sendKeys(faker.lorem().word());
+        Driver.getDriver().switchTo().parentFrame();
+        activeStreamPage.ASPQuestionsTextBox.sendKeys(faker.lorem().sentence());
+        activeStreamPage.ASPAnswerTextBox.sendKeys(faker.lorem().sentence());
+        activeStreamPage.ASPSendButton.click();
     }
 
     @Given("Verifies that new questions have been added")
     public void verifies_that_new_questions_have_been_added() {
-
+        Assert.assertTrue(activeStreamPage.ASPStopButton.isDisplayed());
     }
 
-    @Given("The user adds more than one answer to the questions and clicks the save button.")
+    @Given("Adds multiple answers to the question.")
     public void the_user_adds_more_than_one_answer_to_the_questions_and_clicks_the_save_button() {
-
+        Driver.getDriver().switchTo().frame(activeStreamPage.ASPFrameElement);
+        activeStreamPage.ASPQuestionsTitleTextBox.sendKeys(faker.lorem().word());
+        Driver.getDriver().switchTo().parentFrame();
+        activeStreamPage.ASPQuestionsTextBox.sendKeys(faker.lorem().sentence());
+        activeStreamPage.ASPAnswerTextBox.sendKeys(faker.lorem().sentence());
+        activeStreamPage.ASPAnswer2TextBox.sendKeys(faker.lorem().sentence());
+        activeStreamPage.ASPAnswer3TextBox.sendKeys(faker.lorem().sentence());
+        activeStreamPage.ASPSendButton.click();
     }
 
     @Given("Verifies that multiple answers are attached to questions")
     public void verifies_that_multiple_answers_are_attached_to_questions() {
-
+        Assert.assertTrue(activeStreamPage.ASPAnswerList.size() > 3);
     }
 
     @Given("Click the delete button next to the desired question.")
     public void click_the_delete_button_next_to_the_desired_question() {
 
+        activeStreamPage.APSMoreLinkDeleteTab.click();
+        BrowserUtils.sleep(1);
+
+        Driver.getDriver().switchTo().alert().accept();
+        BrowserUtils.sleep(1);
+
+
     }
 
     @Given("Verify that the requested issue has been deleted.")
     public void verify_that_the_requested_issue_has_been_deleted() {
+        Assert.assertTrue(activeStreamPage.APSSuccessDeletedText.isDisplayed());
+    }
+
+    @Given("Click the more link.")
+    public void click_the_delete_button_next_to_the_desired_replies() {
+        BrowserUtils.sleep(2);
+        activeStreamPage.APSMoreLink.click();
+        BrowserUtils.sleep(1);
 
     }
 
-    @Given("Click the delete button next to the desired replies.")
-    public void click_the_delete_button_next_to_the_desired_replies() {
+    @Given("Click edit link and delete multiple answers.")
+    public void click_edit_link_and_delete_multiple_answers() {
+        activeStreamPage.APSMoreLinkEditTab.click();
+        js.executeScript("window.scrollBy(0, 500);");
+        BrowserUtils.sleep(2);
 
+        activeStreamPage.APSEditAnswerDel();
+        activeStreamPage.APSEditAnswerDel();
+
+        activeStreamPage.APSEditAnswerSendButton.click();
+        BrowserUtils.sleep(2);
 
     }
 
     @Given("Verify that the requested replies has been deleted.")
     public void verify_that_the_requested_replies_has_been_deleted() {
-
+        Assert.assertTrue(activeStreamPage.ASPAnswerList.size() < 2);
     }
 
     @Given("The allow multiple selection checkbox appears and is verified as active.")
     public void the_allow_multiple_selection_checkbox_appears_and_is_verified_as_active() {
-
+        Assert.assertTrue(activeStreamPage.ASPAllowMultipleChoiceCheckbox.isDisplayed());
+        Assert.assertTrue(activeStreamPage.ASPAllowMultipleChoiceCheckbox.isEnabled());
     }
 
-    @Given("It is verified that there are mandatory fields in the survey.")
+    @Given("Verify that there are mandatory fields in the questionnaire.")
     public void it_is_verified_that_there_are_mandatory_fields_in_the_survey() {
 
+        activeStreamPage.ASPQuestionsTextBox.sendKeys(faker.lorem().sentence());
+        activeStreamPage.ASPAnswerTextBox.sendKeys(faker.lorem().sentence());
+        activeStreamPage.ASPSendButton.click();
+        BrowserUtils.sleep(2);
+
+        Assert.assertTrue(activeStreamPage.ASPRequiredFieldTextAlert.isDisplayed());
+    }
+
+    @Given("Click the addMore button.")
+    public void click_the_add_more_button() {
+        activeStreamPage.activeStreamPageAddMoreButton.click();
+        BrowserUtils.sleep(1);
+    }
+
+    @Given("Click the Employees and Departments link.")
+    public void click_the_employees_and_departments_link() {
+        activeStreamPage.employeesAndDepartmentsLink.click();
+        BrowserUtils.sleep(2);
+    }
+
+    @Given("Click multiple selections {int}.")
+    public void click_multiple_selections(int quantity) {
+        for (int i = 0; i < quantity; i++) {
+            activeStreamPage.randomPerson();
+        }
     }
 
 }
