9b7e8c7d8333e97c6b885ed5ec671d20eee9d6cc
Taha <tahacencik@gmail.com>
2023-09-24 12:33:04 +0300

    US02 Updated version.

--- src/test/java/com/crmly/step_def/US_02_Remove_FilesOrImg_Step_Def.java
+++ src/test/java/com/crmly/step_def/US_02_Remove_FilesOrImg_Step_Def.java
@@ -1,15 +1,20 @@
 package com.crmly.step_def;
 
 import com.crmly.pages.US_02_PortalPage;
+import com.crmly.utilities.Driver;
 import io.cucumber.java.en.Given;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class US_02_Rem_FilesOrImg_Step_Def {
+public class US_02_Remove_FilesOrImg_Step_Def {
 
     US_02_PortalPage US02PortalPage = new US_02_PortalPage();
-
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
     @Given("User can remove file after upload and before sending.")
     public void user_can_remove_file_after_upload_and_before_sending() throws InterruptedException {
-        Thread.sleep(1500);
+
+        wait.until(ExpectedConditions.elementToBeClickable(US02PortalPage.deleteBtn));
+
         US02PortalPage.deleteBtn.click();
     }
 
