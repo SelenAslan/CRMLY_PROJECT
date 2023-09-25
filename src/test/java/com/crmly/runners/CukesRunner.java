82fa787113814a59433b90b79466a6b4bea8b96f
ceydakirbas <ceyda.kirbas@hotmail.com>
2023-09-25 21:22:17 +0300

    Company Structure feature updated

--- src/test/java/com/crmly/runners/CukesRunner.java
+++ src/test/java/com/crmly/runners/CukesRunner.java
@@ -7,15 +7,17 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber-reports.json"
+        },
         features = "src/test/resources",
         glue = "com/crmly/step_def",
 
-
-        tags = "@us07",
-
-
-        dryRun = true
-
+        tags = " ",
+        dryRun = false
 
 )
 
