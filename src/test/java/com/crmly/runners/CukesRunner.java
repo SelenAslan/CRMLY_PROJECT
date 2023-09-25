45e9b0dd15c17bf35da967fb8e77691f90b36a55
Gözde Meriç <gozdeekoksal@gmail.com>
2023-09-25 21:23:32 +0300

    Gozde Final Project 21.23

--- src/test/java/com/crmly/runners/CukesRunner.java
+++ src/test/java/com/crmly/runners/CukesRunner.java
@@ -7,16 +7,17 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber-reports.json"
+        },
         features = "src/test/resources",
         glue = "com/crmly/step_def",
 
-        tags = "@Smoke",
-
-       
-
-
+        tags = "@REM-1892 ",
         dryRun = false
-
 
 )
 
